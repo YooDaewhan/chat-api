701f8cbab59e07097a96c8e9323623d8a3cad392
Yoo Daewhan <iok112@naver.com>
2025-06-17 00:13:44 +0900

    db주소 수정

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "ec2-3-36-98-183.ap-northeast-2.compute.amazonaws.com",
+  host: "ec2-3-38-180-108.ap-northeast-2.compute.amazonaws.com",
   user: "ydh960823",
   password: "Adbtmddyd2!",
   database: "ChatLogin",
